4becbcb0bcb231fc3605aa535a99749710302b06
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2022-10-27 14:34:17 +0530

    config: add mainnet defender

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,6 +1,8 @@
 export const attesterAddress: {
   [key: number]: string
 } = {
+  137: "0x8eCEfE7dd4F86D4a96Ff89eBB34C3c6F7871c4c7",
+  56: "0x5FB308DdF9f2df0f2b9916C4b7Ba8915B3a5A565",
   80001: "0x4b53d8d45fe48e0039db40bc21f0a3fc70d0a922",
   421613: "0x39ea4452f2fb4861b28cf1db42a089afbcc1dbd5",
   420: "0x222914bfac6c6f6f10fa1bd38bd5f1d6851bd9ff",
@@ -11,6 +13,8 @@ export const attesterAddress: {
 export const executorAddress: {
   [key: number]: string
 } = {
+  137: "0x8eCEfE7dd4F86D4a96Ff89eBB34C3c6F7871c4c7",
+  56: "0x5FB308DdF9f2df0f2b9916C4b7Ba8915B3a5A565",
   80001: "0x4b53d8d45fe48e0039db40bc21f0a3fc70d0a922",
   421613: "0x39ea4452f2fb4861b28cf1db42a089afbcc1dbd5",
   420: "0x222914bfac6c6f6f10fa1bd38bd5f1d6851bd9ff",
@@ -25,7 +29,9 @@ export const timeout: {
   421613: 7200,
   420: 7200,
   31337: 7200,
-  31338: 7200
+  31338: 7200,
+  137: 7200,
+  56: 7200
 }
 
 export const contractPath: {
