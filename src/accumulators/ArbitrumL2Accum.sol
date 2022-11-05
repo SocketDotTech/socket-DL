4397d0b3779f2d6fe8fc78ba3820ba40f16b8f0f
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2022-11-05 13:36:38 +0530

    feat: l2 accum for arbitrum

--- src/accumulators/ArbitrumL2Accum.sol
+++ src/accumulators/ArbitrumL2Accum.sol
@@ -3,35 +3,34 @@ pragma solidity 0.8.7;
 
 import "./BaseAccum.sol";
 import "../interfaces/INotary.sol";
-import "../interfaces/native-bridge/IInbox.sol";
-import "../interfaces/native-bridge/IOutbox.sol";
+import "../interfaces/native-bridge/IArbSys.sol";
 
-contract ArbitrumAccum is BaseAccum {
-    address public l2Notary;
+contract ArbitrumL2Accum is BaseAccum {
+    address public remoteNotary;
     uint256 public maxSubmissionCost = 1000;
     uint256 public maxGas = 1000;
     uint256 public gasPriceBid = 1000;
     address public remoteRefundAddress = address(1);
     address public callValueRefundAddress = address(2);
-    IInbox public inbox;
 
-    event RetryableTicketCreated(uint256 indexed ticketId);
+    IArbSys constant arbsys = IArbSys(address(100));
+
+    event L2ToL1TxCreated(uint256 indexed withdrawalId);
 
     constructor(
         address socket_,
         address notary_,
-        address inbox_,
-        address l2Notary_,
+        address remoteNotary_,
         uint32 remoteChainSlug_
     ) BaseAccum(socket_, notary_, remoteChainSlug_) {
-        l2Notary = l2Notary_;
-        inbox = IInbox(inbox_);
+        remoteNotary = remoteNotary_;
     }
 
-    function sealPacket(
-        uint256[] calldata bridgeParams,
-        bytes calldata signature_
-    )
+    function setRemoteNotary(address notary_) external onlyOwner {
+        remoteNotary = notary_;
+    }
+
+    function sealPacket(uint256[] calldata, bytes calldata signature_)
         external
         payable
         override
@@ -49,27 +48,12 @@ contract ArbitrumAccum is BaseAccum {
             _roots[_sealedPackets],
             signature_
         );
-        uint256 ticketID = sendL2Message(bridgeParams, data);
 
-        emit RetryableTicketCreated(ticketID);
+        uint256 withdrawalId = arbsys.sendTxToL1(remoteNotary, data);
+
+        emit L2ToL1TxCreated(withdrawalId);
         emit PacketComplete(_roots[_sealedPackets], _sealedPackets);
         return (_roots[_sealedPackets], _sealedPackets++, remoteChainSlug);
-    }
-
-    function sendL2Message(uint256[] calldata bridgeParams, bytes memory data)
-        internal
-        returns (uint256 ticketID)
-    {
-        ticketID = inbox.createRetryableTicket{value: msg.value}(
-            l2Notary,
-            0, // no value needed for attest
-            bridgeParams[0], // maxSubmissionCost
-            remoteRefundAddress,
-            callValueRefundAddress,
-            bridgeParams[1], // maxGas
-            bridgeParams[2], // gasPriceBid
-            data
-        );
     }
 
     function addPackedMessage(bytes32 packedMessage)
