66a0fcbd666f3ea2eea7d034eeda11d7f3106dd8
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2022-11-11 16:20:32 +0530

    feat: optimism accum

---
+++ src/accumulators/native-bridge/optimism/OptimismAccum.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.7;
+
+import "../NativeBridgeAccum.sol";
+import "../../../interfaces/native-bridge/ICrossDomainMessenger.sol";
+
+contract OptimismAccum is NativeBridgeAccum {
+    ICrossDomainMessenger public crossDomainMessenger;
+
+    constructor(
+        address socket_,
+        address notary_,
+        uint32 remoteChainSlug_,
+        uint32 chainSlug_
+    ) NativeBridgeAccum(socket_, notary_, remoteChainSlug_, chainSlug_) {
+        if ((block.chainid == 10 || block.chainid == 420)) {
+            crossDomainMessenger = ICrossDomainMessenger(
+                0x4200000000000000000000000000000000000007
+            );
+        } else {
+            crossDomainMessenger = block.chainid == 1
+                ? ICrossDomainMessenger(
+                    0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1
+                )
+                : ICrossDomainMessenger(
+                    0x5086d1eEF304eb5284A0f6720f79403b4e9bE294
+                );
+        }
+    }
+
+    /**
+     * @param bridgeParams - only one index, gas limit needed to execute data
+     * @param data - encoded data to be sent to remote notary
+     */
+    function sendL2Message(uint256[] calldata bridgeParams, bytes memory data)
+        internal
+        override
+    {
+        crossDomainMessenger.sendMessage(
+            remoteNotary,
+            data,
+            uint32(bridgeParams[0])
+        );
+    }
+}
--- src/notaries/native-bridge/OptimismReceiver.sol
+++ src/notaries/native-bridge/OptimismReceiver.sol
@@ -5,30 +5,31 @@ import "../../interfaces/native-bridge/ICrossDomainMessenger.sol";
 import {NativeBridgeNotary} from "./NativeBridgeNotary.sol";
 
 contract OptimismReceiver is NativeBridgeNotary {
-    address public immutable OVM_L2_CROSS_DOMAIN_MESSENGER;
+    address public OVM_L2_CROSS_DOMAIN_MESSENGER;
     bool public isL2;
 
     modifier onlyRemoteAccumulator() override {
-        if (isL2) {
-            if (
-                msg.sender != OVM_L2_CROSS_DOMAIN_MESSENGER ||
-                ICrossDomainMessenger(OVM_L2_CROSS_DOMAIN_MESSENGER)
-                    .xDomainMessageSender() !=
-                remoteTarget
-            ) revert InvalidSender();
-        } else {
-            // for l2 to l1
-        }
+        if (
+            msg.sender != OVM_L2_CROSS_DOMAIN_MESSENGER &&
+            ICrossDomainMessenger(OVM_L2_CROSS_DOMAIN_MESSENGER)
+                .xDomainMessageSender() !=
+            remoteTarget
+        ) revert InvalidSender();
         _;
     }
 
     constructor(
-        address ovmL2CrossDomainMessenger,
         address signatureVerifier_,
         uint32 chainSlug_,
         address remoteTarget_
     ) NativeBridgeNotary(signatureVerifier_, chainSlug_, remoteTarget_) {
-        isL2 = (block.chainid == 10 || block.chainid == 420) ? true : false;
-        OVM_L2_CROSS_DOMAIN_MESSENGER = ovmL2CrossDomainMessenger;
+        if ((block.chainid == 10 || block.chainid == 420)) {
+            isL2 = true;
+            OVM_L2_CROSS_DOMAIN_MESSENGER = 0x4200000000000000000000000000000000000007;
+        } else {
+            OVM_L2_CROSS_DOMAIN_MESSENGER = block.chainid == 1
+                ? 0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1
+                : 0x5086d1eEF304eb5284A0f6720f79403b4e9bE294;
+        }
     }
 }
