0d21d9e514e11eb70002ff692d368dd6a5fa38f4
arthcp <arth.c.patel@gmail.com>
2022-12-25 01:36:11 +0530

    feat: hashchain accum, deaccum

---
+++ contracts/deaccumulators/HashChainDeaccum.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.7;
+
+import "../interfaces/IDeaccumulator.sol";
+
+contract HashChainDeaccum is IDeaccumulator {
+    /// returns if the packed message is the part of a merkle tree or not
+    /// @inheritdoc IDeaccumulator
+    function verifyMessageInclusion(
+        bytes32 root_,
+        bytes32 packedMessage_,
+        bytes calldata proof
+    ) external pure override returns (bool) {
+        bytes32[] memory chain = abi.decode(proof, (bytes32[]));
+        uint256 len = chain.length;
+        bytes32 generatedRoot;
+        for (uint256 i = 0; i < len; i++) {
+            generatedRoot = keccak256(abi.encode(generatedRoot, chain[i]));
+        }
+        generatedRoot = keccak256(abi.encode(generatedRoot, packedMessage_));
+        return root_ == generatedRoot;
+    }
+}
