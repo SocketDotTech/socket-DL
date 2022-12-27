369be89b515d4f29b2105a5c70ddcb3124de0a83
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2022-12-28 01:00:17 +0530

    feat: optimistic switchboard

---
+++ contracts/switchboard/OptimisticSwitchboard.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.7;
+
+import "../interfaces/ISwitchboard.sol";
+import "../interfaces/ISocket.sol";
+import "../utils/AccessControl.sol";
+
+contract OptimisticSwitchboard is ISwitchboard, AccessControl {
+    ISocket public socket;
+    uint256 public immutable timeoutInSeconds;
+    uint256 public immutable chainSlug;
+
+    bool tripFuse;
+
+    // TODO: change name
+    // TODO: should we use packetId here?
+    // root => isPaused
+    mapping(bytes32 => bool) public isRootPaused;
+
+    event SocketSet(address newSocket_);
+    event SwitchboardTripped(bool tripFuse_);
+
+    error TransferFailed();
+    error FeesNotEnough();
+
+    constructor(
+        address owner_,
+        address socket_,
+        uint32 chainSlug_,
+        uint256 timeoutInSeconds_
+    ) AccessControl(owner_) {
+        chainSlug = chainSlug_;
+        socket = ISocket(socket_);
+
+        // TODO: restrict the timeout durations to a few select options
+        timeoutInSeconds = timeoutInSeconds_;
+    }
+
+    function payFees(
+        uint256 msgGasLimit,
+        uint256 remoteChainSlug
+    ) external payable override {
+        // TODO: updated with issue #45
+        uint256 expectedFees = 0;
+        if (msg.value != expectedFees) revert FeesNotEnough();
+    }
+
+    /**
+     * @notice verifies if the packet satisfies needed checks before execution
+     * @param root root
+     * @param proposeTime time at which packet was proposed
+     */
+    function allowPacket(
+        bytes32 root,
+        uint256 proposeTime
+    ) external view override returns (bool) {
+        if (tripFuse || isRootPaused[root]) return false;
+        if (block.timestamp - proposeTime < timeoutInSeconds) return false;
+        return true;
+    }
+
+    /**
+     * @notice updates socket_
+     * @param socket_ address of Notary
+     */
+    function setSocket(address socket_) external onlyOwner {
+        socket = ISocket(socket_);
+        emit SocketSet(socket_);
+    }
+
+    /**
+     * @notice pause/unpause execution
+     * @param tripFuse_ bool indicating verification is active or not
+     */
+    function trip(bool tripFuse_) external onlyOwner {
+        tripFuse = tripFuse_;
+        emit SwitchboardTripped(tripFuse_);
+    }
+
+    // TODO: to support fee distribution
+    /**
+     * @notice transfers the fees collected to `account_`
+     * @param account_ address to transfer ETH
+     */
+    function withdrawFees(address account_) external onlyOwner {
+        require(account_ != address(0));
+        (bool success, ) = account_.call{value: address(this).balance}("");
+        if (!success) revert TransferFailed();
+    }
+
+    /**
+     * @notice adds an watcher for `remoteChainSlug_` chain
+     * @param remoteChainSlug_ remote chain slug
+     * @param watcher_ watcher address
+     */
+    function grantWatcherRole(
+        uint256 remoteChainSlug_,
+        address watcher_
+    ) external onlyOwner {
+        _grantRole(_watcherRole(remoteChainSlug_), watcher_);
+    }
+
+    /**
+     * @notice removes an watcher from `remoteChainSlug_` chain list
+     * @param remoteChainSlug_ remote chain slug
+     * @param watcher_ watcher address
+     */
+    function revokeWatcherRole(
+        uint256 remoteChainSlug_,
+        address watcher_
+    ) external onlyOwner {
+        _revokeRole(_watcherRole(remoteChainSlug_), watcher_);
+    }
+
+    function _watcherRole(uint256 chainSlug_) internal pure returns (bytes32) {
+        return bytes32(chainSlug_);
+    }
+}
