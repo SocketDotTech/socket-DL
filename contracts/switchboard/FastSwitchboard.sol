52c4b5886a0d12f616848515a53d6764baf8b580
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2022-12-28 16:56:12 +0530

    fix: fast switchboard for multiple chains

--- contracts/switchboard/FastSwitchboard.sol
+++ contracts/switchboard/FastSwitchboard.sol
@@ -9,25 +9,20 @@ contract FastSwitchboard is ISwitchboard, AccessControl {
     ISocket public socket;
     uint256 public immutable chainSlug;
     uint256 public timeoutInSeconds;
+    bool public tripFuse;
 
-    // keccak256("WATCHER")
-    bytes32 WATCHER_ROLE =
-        0xc5f1d4258c62728075c120634c70b3363b74519d0ae4b53891a4b74fe4bfa0b8;
+    // dst chain slug => total watchers registered
+    mapping(uint256 => uint256) public totalWatchers;
 
-    bool tripFuse;
+    // attester => packetId => is attested
+    mapping(address => mapping(uint256 => bool)) public isAttested;
 
-    // total watchers registered
-    uint256 public totalWatchers;
-
-    // attester => root => is attested
-    mapping(address => mapping(bytes32 => bool)) public isAttested;
-
-    // root => total attestations
-    mapping(bytes32 => uint256) private attestations;
+    // packetId => total attestations
+    mapping(uint256 => uint256) public attestations;
 
     event SocketSet(address newSocket_);
     event SwitchboardTripped(bool tripFuse_);
-    event RootAttested(bytes32 root, address attester);
+    event PacketAttested(uint256 packetId, address attester);
 
     error TransferFailed();
     error FeesNotEnough();
@@ -47,19 +42,20 @@ contract FastSwitchboard is ISwitchboard, AccessControl {
         timeoutInSeconds = timeoutInSeconds_;
     }
 
-    function attest(bytes32 root) external {
-        if (!_hasRole(WATCHER_ROLE, msg.sender)) revert WatcherNotFound();
-        if (isAttested[msg.sender][root]) revert AlreadyAttested();
+    function attest(uint256 packetId, uint256 srcChainSlug) external {
+        if (isAttested[msg.sender][packetId]) revert AlreadyAttested();
+        if (!_hasRole(_watcherRole(srcChainSlug), msg.sender))
+            revert WatcherNotFound();
 
-        isAttested[msg.sender][root] = true;
-        attestations[root]++;
+        isAttested[msg.sender][packetId] = true;
+        attestations[packetId]++;
 
-        emit RootAttested(root, msg.sender);
+        emit PacketAttested(packetId, msg.sender);
     }
 
     function payFees(
         uint256 msgGasLimit,
-        uint256 remoteChainSlug
+        uint256 dstChainSlug
     ) external payable override {
         // TODO: updated with issue #45
         uint256 expectedFees = 0;
@@ -69,19 +65,22 @@ contract FastSwitchboard is ISwitchboard, AccessControl {
     // todo: switchboard might need src chain slug and packet id while verifying details here?
     /**
      * @notice verifies if the packet satisfies needed checks before execution
-     * @param root root
+     * @param packetId packetId
      * @param proposeTime time at which packet was proposed
      */
     function allowPacket(
-        bytes32 root,
+        bytes32,
+        uint256 packetId,
+        uint256 srcChainSlug,
         uint256 proposeTime
     ) external view override returns (bool) {
         if (tripFuse) return false;
-        if (block.timestamp - proposeTime > timeoutInSeconds) return true;
 
-        // this should be chain dependent? (need src chain slug)
-        if (attestations[root] != totalWatchers) return false;
-        return true;
+        // to handle the situation if a watcher is removed after it attested the packet
+        if (attestations[packetId] >= totalWatchers[srcChainSlug]) return true;
+
+        if (block.timestamp - proposeTime >= timeoutInSeconds) return true;
+        return false;
     }
 
     /**
@@ -114,22 +113,36 @@ contract FastSwitchboard is ISwitchboard, AccessControl {
     }
 
     /**
-     * @notice adds a watcher for `remoteChainSlug_` chain
+     * @notice adds a watcher for `srcChainSlug_` chain
      * @param watcher_ watcher address
      */
-    function grantWatcherRole(address watcher_) external onlyOwner {
-        if (_hasRole(WATCHER_ROLE, watcher_)) revert WatcherFound();
-        _grantRole(WATCHER_ROLE, watcher_);
-        totalWatchers++;
+    function grantWatcherRole(
+        uint256 srcChainSlug_,
+        address watcher_
+    ) external onlyOwner {
+        if (_hasRole(_watcherRole(srcChainSlug_), watcher_))
+            revert WatcherFound();
+
+        _grantRole(_watcherRole(srcChainSlug_), watcher_);
+        totalWatchers[srcChainSlug_]++;
     }
 
     /**
-     * @notice removes a watcher from `remoteChainSlug_` chain list
+     * @notice removes a watcher from `srcChainSlug_` chain list
      * @param watcher_ watcher address
      */
-    function revokeWatcherRole(address watcher_) external onlyOwner {
-        if (!_hasRole(WATCHER_ROLE, watcher_)) revert WatcherNotFound();
-        _revokeRole(WATCHER_ROLE, watcher_);
-        totalWatchers--;
+    function revokeWatcherRole(
+        uint256 srcChainSlug_,
+        address watcher_
+    ) external onlyOwner {
+        if (!_hasRole(_watcherRole(srcChainSlug_), watcher_))
+            revert WatcherNotFound();
+
+        _revokeRole(_watcherRole(srcChainSlug_), watcher_);
+        totalWatchers[srcChainSlug_]--;
+    }
+
+    function _watcherRole(uint256 chainSlug_) internal pure returns (bytes32) {
+        return bytes32(chainSlug_);
     }
 }
