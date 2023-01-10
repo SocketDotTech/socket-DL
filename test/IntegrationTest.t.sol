430f9991e95a6a04b4e70a4eaed993c0f63736be
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-01-10 16:40:39 +0530

    test: end to end socket test

--- test/IntegrationTest.t.sol
+++ test/IntegrationTest.t.sol
@@ -7,10 +7,9 @@ import "../contracts/examples/Counter.sol";
 contract HappyTest is Setup {
     Counter srcCounter__;
     Counter dstCounter__;
-    uint256 minFees = 10000;
+
     uint256 addAmount = 100;
     uint256 subAmount = 40;
-
     bool isFast = true;
 
     event ExecutionSuccess(uint256 msgId);
@@ -19,9 +18,9 @@ contract HappyTest is Setup {
 
     function setUp() external {
         uint256[] memory attesters = new uint256[](1);
-        attesters[0] = _attesterPrivateKey;
+        attesters[0] = _transmitterPrivateKey;
 
-        _dualChainSetup(attesters, minFees);
+        _dualChainSetup(attesters);
         _deployPlugContracts();
         _configPlugContracts(isFast);
     }
@@ -30,17 +29,20 @@ contract HappyTest is Setup {
         uint256 amount = 100;
         bytes memory payload = abi.encode(keccak256("OP_ADD"), amount);
         bytes memory proof = abi.encode(0);
-        address capacitor = isFast
-            ? address(_a.fastCapacitor__)
-            : address(_a.slowCapacitor__);
 
-        hoax(_raju);
-        srcCounter__.remoteAddOperation{value: minFees}(
-            _b.chainSlug,
-            amount,
-            _msgGasLimit
-        );
-        // TODO: get nonce from event
+        uint256 index = isFast ? 0 : 1;
+        address capacitor = address(_a.configs__[index].capacitor__);
+        {
+            uint256 switchboardFees = SwitchboardBase(
+                address(_a.configs__[index].switchboard__)
+            ).getMinFees(_msgGasLimit, _b.chainSlug);
+            uint256 socketFees = _a.transmitManager__.getMinFees(_b.chainSlug);
+
+            hoax(_raju);
+            srcCounter__.remoteAddOperation{
+                value: switchboardFees + socketFees
+            }(_b.chainSlug, amount, _msgGasLimit);
+        }
 
         uint256 msgId = _packMessageId(_a.chainSlug, 0);
 
@@ -49,63 +51,10 @@ contract HappyTest is Setup {
             uint256 packetId,
             bytes memory sig
         ) = _getLatestSignature(_a, capacitor, _b.chainSlug);
-        address attester = vm.addr(_attesterPrivateKey);
-
-        // not an attester
-        {
-            hoax(_socketOwner);
-            _a.notary__.revokeAttesterRole(_b.chainSlug, attester);
-            vm.expectRevert(INotary.InvalidAttester.selector);
-            _sealOnSrc(_a, capacitor, sig);
-
-            hoax(_socketOwner);
-            _a.notary__.grantAttesterRole(_b.chainSlug, attester);
-        }
+        address attester = vm.addr(_transmitterPrivateKey);
 
         _sealOnSrc(_a, capacitor, sig);
-
-        // revert execution if packet not proposed
-        assertEq(uint256(_b.notary__.getPacketStatus(packetId)), 0);
-
-        vm.expectRevert(SocketDst.VerificationFailed.selector);
-        _executePayloadOnDst(
-            _a,
-            _b,
-            address(dstCounter__),
-            packetId,
-            msgId,
-            _msgGasLimit,
-            payload,
-            proof
-        );
-
-        _submitRootOnDst(_b, sig, packetId, root);
-
-        vm.expectRevert(INotary.AlreadyAttested.selector);
-        _submitRootOnDst(_b, sig, packetId, root);
-
-        // without executor role
-        {
-            hoax(_socketOwner);
-            _b.socket__.revokeExecutorRole(_raju);
-
-            vm.expectRevert();
-            _executePayloadOnDst(
-                _a,
-                _b,
-                address(dstCounter__),
-                packetId,
-                msgId,
-                _msgGasLimit,
-                payload,
-                proof
-            );
-
-            hoax(_socketOwner);
-            _b.socket__.grantExecutorRole(_raju);
-        }
-
-        assertEq(uint256(_b.socket__.messageStatus(msgId)), 0);
+        _proposeOnDst(_b, sig, packetId, root);
 
         vm.expectEmit(true, false, false, false);
         emit ExecutionSuccess(msgId);
@@ -142,8 +91,10 @@ contract HappyTest is Setup {
         bytes memory payload = abi.encode(keccak256("OP_ADD"), amount);
         bytes memory proof = abi.encode(0);
         address capacitor = isFast
-            ? address(_b.fastCapacitor__)
-            : address(_b.slowCapacitor__);
+            ? address(_b.configs__[0].capacitor__)
+            : address(_b.configs__[1].capacitor__);
+
+        uint256 minFees = _b.transmitManager__.getMinFees(_a.chainSlug);
 
         hoax(_raju);
         dstCounter__.remoteAddOperation{value: minFees}(
@@ -160,7 +111,7 @@ contract HappyTest is Setup {
 
         uint256 msgId = _packMessageId(_b.chainSlug, 0);
         _sealOnSrc(_b, capacitor, sig);
-        _submitRootOnDst(_a, sig, packetId, root);
+        _proposeOnDst(_a, sig, packetId, root);
 
         _executePayloadOnDst(
             _b,
@@ -177,315 +128,6 @@ contract HappyTest is Setup {
         assertEq(dstCounter__.counter(), 0);
     }
 
-    function testRemoteAddAndSubtract() external {
-        bytes memory addPayload = abi.encode(keccak256("OP_ADD"), addAmount);
-        uint256 addMsgId = _packMessageId(_a.chainSlug, 0);
-
-        bytes memory subPayload = abi.encode(keccak256("OP_SUB"), subAmount);
-        uint256 subMsgId = _packMessageId(_a.chainSlug, 1);
-        address capacitor = isFast
-            ? address(_a.fastCapacitor__)
-            : address(_a.slowCapacitor__);
-
-        bytes32 root;
-        uint256 packetId;
-        bytes memory sig;
-
-        hoax(_raju);
-        vm.expectRevert(Vault.InsufficientFees.selector);
-        srcCounter__.remoteAddOperation(_b.chainSlug, addAmount, _msgGasLimit);
-
-        hoax(_raju);
-        srcCounter__.remoteAddOperation{value: minFees}(
-            _b.chainSlug,
-            addAmount,
-            _msgGasLimit
-        );
-
-        (root, packetId, sig) = _getLatestSignature(
-            _a,
-            capacitor,
-            _b.chainSlug
-        );
-        _sealOnSrc(_a, capacitor, sig);
-        _submitRootOnDst(_b, sig, packetId, root);
-
-        _executePayloadOnDst(
-            _a,
-            _b,
-            address(dstCounter__),
-            packetId,
-            addMsgId,
-            _msgGasLimit,
-            addPayload,
-            abi.encode(0)
-        );
-
-        hoax(_raju);
-        srcCounter__.remoteSubOperation{value: minFees}(
-            _b.chainSlug,
-            subAmount,
-            _msgGasLimit
-        );
-
-        (root, packetId, sig) = _getLatestSignature(
-            _a,
-            capacitor,
-            _b.chainSlug
-        );
-        _sealOnSrc(_a, capacitor, sig);
-        _submitRootOnDst(_b, sig, packetId, root);
-
-        _executePayloadOnDst(
-            _a,
-            _b,
-            address(dstCounter__),
-            packetId,
-            subMsgId,
-            _msgGasLimit,
-            subPayload,
-            abi.encode(0)
-        );
-
-        assertEq(dstCounter__.counter(), addAmount - subAmount);
-        assertEq(srcCounter__.counter(), 0);
-    }
-
-    function testExecSameMessageTwice() external {
-        uint256 amount = 100;
-        bytes memory payload = abi.encode(keccak256("OP_ADD"), amount);
-        bytes memory proof = abi.encode(0);
-        uint256 msgId = _packMessageId(_a.chainSlug, 0);
-        address capacitor = isFast
-            ? address(_a.fastCapacitor__)
-            : address(_a.slowCapacitor__);
-
-        hoax(_raju);
-        srcCounter__.remoteAddOperation{value: minFees}(
-            _b.chainSlug,
-            amount,
-            _msgGasLimit
-        );
-        (
-            bytes32 root,
-            uint256 packetId,
-            bytes memory sig
-        ) = _getLatestSignature(_a, capacitor, _b.chainSlug);
-        _sealOnSrc(_a, capacitor, sig);
-        _submitRootOnDst(_b, sig, packetId, root);
-
-        _executePayloadOnDst(
-            _a,
-            _b,
-            address(dstCounter__),
-            packetId,
-            msgId,
-            _msgGasLimit,
-            payload,
-            proof
-        );
-
-        vm.expectRevert(SocketDst.MessageAlreadyExecuted.selector);
-        _executePayloadOnDst(
-            _a,
-            _b,
-            address(dstCounter__),
-            packetId,
-            msgId,
-            _msgGasLimit,
-            payload,
-            proof
-        );
-
-        assertEq(dstCounter__.counter(), amount);
-        assertEq(srcCounter__.counter(), 0);
-    }
-
-    function testExecuteWithLowGasLimit() external {
-        uint256 amount = 100;
-        bytes memory payload = abi.encode(keccak256("OP_ADD"), amount);
-        bytes memory proof = abi.encode(0);
-        uint256 msgId = _packMessageId(_a.chainSlug, 0);
-        address capacitor = isFast
-            ? address(_a.fastCapacitor__)
-            : address(_a.slowCapacitor__);
-
-        // providing a lower gas limit
-        uint256 msgGasLimit = 1000;
-        hoax(_raju);
-        srcCounter__.remoteAddOperation{value: minFees}(
-            _b.chainSlug,
-            amount,
-            msgGasLimit
-        );
-
-        (uint256 packetId, ) = _attesterChecks(capacitor);
-
-        assertEq(uint256(_b.socket__.messageStatus(msgId)), 0);
-
-        // ExecutionFailedBytes with out of gas
-        vm.expectEmit(true, true, false, false);
-        emit ExecutionFailedBytes(msgId, "0x");
-
-        _executePayloadOnDst(
-            _a,
-            _b,
-            address(dstCounter__),
-            packetId,
-            msgId,
-            msgGasLimit,
-            payload,
-            proof
-        );
-
-        assertEq(uint256(_b.socket__.messageStatus(msgId)), 2);
-    }
-
-    function testExecuteWithExecutionFailure() external {
-        uint256 amount = 100;
-        bytes memory payload = abi.encode(keccak256("OP_SUB"), amount);
-        bytes memory proof = abi.encode(0);
-        uint256 msgId = _packMessageId(_a.chainSlug, 0);
-        address capacitor = isFast
-            ? address(_a.fastCapacitor__)
-            : address(_a.slowCapacitor__);
-
-        hoax(_raju);
-        srcCounter__.remoteSubOperation{value: minFees}(
-            _b.chainSlug,
-            amount,
-            _msgGasLimit
-        );
-
-        (uint256 packetId, ) = _attesterChecks(capacitor);
-        assertEq(uint256(_b.socket__.messageStatus(msgId)), 0);
-
-        // ExecutionFailed with error decoded as string
-        vm.expectEmit(true, true, false, false);
-        emit ExecutionFailed(msgId, "CounterMock: Subtraction Overflow");
-
-        _executePayloadOnDst(
-            _a,
-            _b,
-            address(dstCounter__),
-            packetId,
-            msgId,
-            _msgGasLimit,
-            payload,
-            proof
-        );
-
-        assertEq(uint256(_b.socket__.messageStatus(msgId)), 2);
-    }
-
-    function testRemoteAddFromAtoBFastPath() external {
-        uint256 amount = 100;
-        bytes memory payload = abi.encode(keccak256("OP_ADD"), amount);
-        bytes memory proof = abi.encode(0);
-        address capacitor = address(_a.fastCapacitor__);
-
-        hoax(_raju);
-        srcCounter__.remoteAddOperation{value: minFees}(
-            _b.chainSlug,
-            amount,
-            _msgGasLimit
-        );
-
-        uint256 msgId = _packMessageId(_a.chainSlug, 0);
-
-        // add attesters
-        address newAttester = vm.addr(uint256(10));
-        hoax(_socketOwner);
-        _b.notary__.grantAttesterRole(_a.chainSlug, newAttester);
-
-        (uint256 packetId, bytes32 root) = _attesterChecks(capacitor);
-
-        // get signature
-        bytes memory sig = _createSignature(
-            _b.chainSlug,
-            packetId,
-            uint256(10),
-            root
-        );
-
-        vm.expectRevert(SocketDst.VerificationFailed.selector);
-        _executePayloadOnDst(
-            _a,
-            _b,
-            address(dstCounter__),
-            packetId,
-            msgId,
-            _msgGasLimit,
-            payload,
-            proof
-        );
-
-        // attest
-        hoax(newAttester);
-        _b.notary__.attest(packetId, root, sig);
-
-        _executePayloadOnDst(
-            _a,
-            _b,
-            address(dstCounter__),
-            packetId,
-            msgId,
-            _msgGasLimit,
-            payload,
-            proof
-        );
-
-        assertEq(dstCounter__.counter(), amount);
-        assertEq(srcCounter__.counter(), 0);
-    }
-
-    function testRemoteAddFromAtoBSlowPath() external {
-        uint256 amount = 100;
-        bytes memory payload = abi.encode(keccak256("OP_ADD"), amount);
-        bytes memory proof = abi.encode(0);
-        address capacitor = address(_a.slowCapacitor__);
-        _configPlugContracts(false);
-
-        hoax(_raju);
-        srcCounter__.remoteAddOperation{value: minFees}(
-            _b.chainSlug,
-            amount,
-            _msgGasLimit
-        );
-
-        uint256 msgId = _packMessageId(_a.chainSlug, 0);
-
-        (uint256 packetId, ) = _attesterChecks(capacitor);
-
-        vm.expectRevert(SocketDst.VerificationFailed.selector);
-        _executePayloadOnDst(
-            _a,
-            _b,
-            address(dstCounter__),
-            packetId,
-            msgId,
-            _msgGasLimit,
-            payload,
-            proof
-        );
-
-        vm.warp(block.timestamp + _slowCapacitorWaitTime);
-
-        _executePayloadOnDst(
-            _a,
-            _b,
-            address(dstCounter__),
-            packetId,
-            msgId,
-            _msgGasLimit,
-            payload,
-            proof
-        );
-
-        assertEq(dstCounter__.counter(), amount);
-        assertEq(srcCounter__.counter(), 0);
-    }
-
     function _deployPlugContracts() internal {
         vm.startPrank(_plugOwner);
 
@@ -497,22 +139,20 @@ contract HappyTest is Setup {
     }
 
     function _configPlugContracts(bool isFast_) internal {
-        string memory integrationType = isFast_
-            ? fastIntegrationType
-            : slowIntegrationType;
+        uint256 index = isFast_ ? 0 : 1;
 
         hoax(_plugOwner);
         srcCounter__.setSocketConfig(
             _b.chainSlug,
             address(dstCounter__),
-            address(0) // integrationType // TODO: change to switchboard
+            address(_a.configs__[index].switchboard__)
         );
 
         hoax(_plugOwner);
         dstCounter__.setSocketConfig(
             _a.chainSlug,
             address(srcCounter__),
-            address(0) // integrationType // TODO: change to switchboard
+            address(_b.configs__[index].switchboard__)
         );
     }
 
@@ -526,6 +166,6 @@ contract HappyTest is Setup {
             _b.chainSlug
         );
         _sealOnSrc(_a, capacitor, sig);
-        _submitRootOnDst(_b, sig, packetId, root);
+        _proposeOnDst(_b, sig, packetId, root);
     }
 }
