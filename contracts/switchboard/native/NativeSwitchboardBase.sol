d07866ebdb0007fafb80d8a487196f06334b5989
arthcp <arth.c.patel@gmail.com>
2023-01-12 17:42:21 +0400

    feat: add getMinFees to interface

--- contracts/switchboard/native/NativeSwitchboardBase.sol
+++ contracts/switchboard/native/NativeSwitchboardBase.sol
@@ -36,14 +36,14 @@ abstract contract NativeSwitchboardBase is ISwitchboard, AccessControl {
     function getMinFees(
         uint256 msgGasLimit,
         uint256 dstChainSlug
-    ) external returns (uint256) {
+    ) external view override returns (uint256) {
         return _calculateFees(msgGasLimit, dstChainSlug);
     }
 
     function _calculateFees(
         uint256 msgGasLimit,
         uint256 dstChainSlug
-    ) internal returns (uint256 expectedFees) {
+    ) internal view returns (uint256 expectedFees) {
         uint256 dstRelativeGasPrice = oracle.relativeGasPrice(dstChainSlug);
 
         uint256 minExecutionFees = _getExecutionFees(
