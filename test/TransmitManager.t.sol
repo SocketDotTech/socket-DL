c789536f040a2637d16e4332787ecb72e07d80a3
kanth <kanth.explorer@gmail.com>
2023-02-18 03:14:49 +0530

    feat: Fees-Tests for TransmitManager

--- test/TransmitManager.t.sol
+++ test/TransmitManager.t.sol
@@ -10,13 +10,26 @@ import {SignatureVerifier} from "../contracts/utils/SignatureVerifier.sol";
 
 contract TransmitManagerTest is Test {
     GasPriceOracle internal gasPriceOracle;
+
     uint256 chainSlug = uint32(uint256(0x2013AA263));
     uint256 destChainSlug = uint32(uint256(0x2013AA264));
+
     uint256 internal c = 1;
-    address immutable owner = address(uint160(c++));
-    address immutable transmitter = address(uint160(c++));
-    address immutable nonTransmitter = address(uint160(c++));
-    uint256 gasLimit = 200000;
+
+    uint256 immutable ownerPrivateKey = c++;
+    address owner;
+
+    uint256 immutable transmitterPrivateKey = c++;
+    address transmitter;
+
+    uint256 immutable nonTransmitterPrivateKey = c++;
+    address nonTransmitter;
+
+    uint256 sealGasLimit = 200000;
+    uint256 proposeGasLimit = 100000;
+    uint256 sourceGasPrice = 1200000;
+    uint256 relativeGasPrice = 1100000;
+
     SignatureVerifier internal signatureVerifier;
     TransmitManager internal transmitManager;
 
@@ -26,6 +39,10 @@ contract TransmitManagerTest is Test {
     error TransmitterNotFound();
 
     function setUp() public {
+        owner = vm.addr(ownerPrivateKey);
+        transmitter = vm.addr(transmitterPrivateKey);
+        nonTransmitter = vm.addr(nonTransmitterPrivateKey);
+
         gasPriceOracle = new GasPriceOracle(owner, chainSlug);
         signatureVerifier = new SignatureVerifier();
         transmitManager = new TransmitManager(
@@ -33,8 +50,21 @@ contract TransmitManagerTest is Test {
             gasPriceOracle,
             owner,
             chainSlug,
-            gasLimit
+            sealGasLimit
         );
+
+        vm.startPrank(owner);
+        gasPriceOracle.setTransmitManager(transmitManager);
+        transmitManager.grantTransmitterRole(chainSlug, transmitter);
+        transmitManager.grantTransmitterRole(destChainSlug, transmitter);
+        transmitManager.setProposeGasLimit(destChainSlug, proposeGasLimit);
+        vm.stopPrank();
+
+        vm.startPrank(transmitter);
+        gasPriceOracle.setSourceGasPrice(sourceGasPrice);
+        gasPriceOracle.setRelativeGasPrice(destChainSlug, relativeGasPrice);
+
+        vm.stopPrank();
     }
 
     function testGrantTransmitterRole() public {
@@ -43,5 +73,68 @@ contract TransmitManagerTest is Test {
         vm.stopPrank();
 
         assertTrue(transmitManager.isTransmitter(transmitter, chainSlug));
+    }
+
+    function testGenerateAndVerifySignature() public {
+        uint256 packetId = 123;
+        bytes32 root = bytes32(abi.encode(123));
+        bytes32 digest = keccak256(abi.encode(chainSlug, packetId, root));
+        bytes memory sig = _createSignature(digest, transmitterPrivateKey);
+
+        address transmitter_Decoded = signatureVerifier.recoverSigner(
+            chainSlug,
+            packetId,
+            root,
+            sig
+        );
+
+        assertEq(transmitter, transmitter_Decoded);
+    }
+
+    function testCheckTransmitter() public {
+        uint256 packetId = 123;
+        bytes32 root = bytes32(abi.encode(123));
+        bytes32 digest = keccak256(abi.encode(chainSlug, packetId, root));
+        bytes memory sig = _createSignature(digest, transmitterPrivateKey);
+
+        (address transmitter_Rsp, bool isTransmitter) = transmitManager
+            .checkTransmitter(
+                (chainSlug << 128) | chainSlug,
+                packetId,
+                root,
+                sig
+            );
+        assertEq(transmitter_Rsp, transmitter);
+        assertTrue(isTransmitter);
+    }
+
+    function testGetMinFees() public {
+        uint256 minFees = transmitManager.getMinFees(destChainSlug);
+
+        // sealGasLimit * sourceGasPrice + proposeGasLimit * relativeGasPrice
+        uint256 minFees_Expected = sealGasLimit *
+            sourceGasPrice +
+            proposeGasLimit *
+            relativeGasPrice;
+        assertEq(minFees, minFees_Expected);
+    }
+
+    function _createSignature(
+        bytes32 digest_,
+        uint256 privateKey_
+    ) internal returns (bytes memory sig) {
+        bytes32 digest = keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", digest_)
+        );
+
+        (uint8 sigV, bytes32 sigR, bytes32 sigS) = vm.sign(privateKey_, digest);
+        sig = new bytes(65);
+        bytes1 v32 = bytes1(sigV);
+
+        assembly {
+            mstore(add(sig, 96), v32)
+            mstore(add(sig, 32), sigR)
+            mstore(add(sig, 64), sigS)
+        }
     }
 }
