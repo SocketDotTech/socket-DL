6d649050c0da281ba4a0438d297b4726f875bd74
kanth <kanth.explorer@gmail.com>
2023-02-20 13:08:41 +0530

    refactor: move FastSwitchBoardTest to new directory uner tests

--- test/switchboard/default-switchboards/FastSwitchboard.t.sol
+++ test/switchboard/default-switchboards/FastSwitchboard.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "./Setup.t.sol";
+import "../../Setup.t.sol";
 
 contract FastSwitchboardTest is Setup {
     bool isFast = true;
