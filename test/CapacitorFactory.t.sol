508403970e112843ebece1fd0d9211a6267b023c
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-02-23 21:04:38 +0530

    test: capacitor factory

---
+++ test/CapacitorFactory.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+import "../contracts/CapacitorFactory.sol";
+
+contract CapacitorFactoryTest is Test {
+    uint256 internal c = 1;
+    address immutable _owner = address(uint160(c++));
+    address immutable _raju = address(uint160(c++));
+
+    uint256 tokenSupply = 10000;
+    uint256 siblingChainSlug = 1;
+
+    CapacitorFactory _cf;
+    ERC20PresetFixedSupply _token;
+
+    function setUp() external {
+        hoax(_owner);
+        _cf = new CapacitorFactory();
+        _token = new ERC20PresetFixedSupply("TEST", "T", tokenSupply, _owner);
+    }
+
+    function testDeploy(uint256 capacitorType) external {
+        if (capacitorType != 1 && capacitorType != 2) {
+            vm.expectRevert(ICapacitorFactory.InvalidCapacitorType.selector);
+        }
+        _cf.deploy(capacitorType, siblingChainSlug);
+    }
+
+    function testRescueFunds() external {
+        uint256 amount = 1000;
+
+        hoax(_owner);
+        _token.transfer(address(_cf), amount);
+
+        uint256 initialBalOfOwner = _token.balanceOf(_raju);
+
+        hoax(_raju);
+        vm.expectRevert(Ownable.OnlyOwner.selector);
+        _cf.rescueFunds(address(_token), _raju, amount);
+
+        hoax(_owner);
+        _cf.rescueFunds(address(_token), _raju, amount);
+
+        uint256 finalBalOfOwner = _token.balanceOf(_raju);
+        assertEq(finalBalOfOwner - initialBalOfOwner, amount);
+    }
+}
