ad6df20d3b3d6b217ca5219d347728106ad43ffc
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-02-23 21:06:55 +0530

    test: capacitors

---
+++ test/HashChainCapacitor.t.sol
@@ -0,0 +1,162 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../contracts/capacitors/HashChainCapacitor.sol";
+import "../contracts/decapacitors/HashChainDecapacitor.sol";
+
+contract HashChainCapacitorTest is Test {
+    uint256 internal c = 1;
+    address immutable _owner = address(uint160(c++));
+    address immutable _socket = address(uint160(c++));
+    address immutable _raju = address(uint160(c++));
+    bytes32 immutable _message_0 = bytes32(c++);
+    bytes32 immutable _message_1 = bytes32(c++);
+    bytes32 immutable _message_2 = bytes32(c++);
+    bytes32[] internal roots;
+
+    HashChainCapacitor _hcCapacitor;
+    HashChainDecapacitor _hcDecapacitor;
+
+    function setUp() external {
+        hoax(_owner);
+        _hcCapacitor = new HashChainCapacitor(_socket);
+        _hcDecapacitor = new HashChainDecapacitor();
+    }
+
+    function testSetUp() external {
+        assertEq(_hcCapacitor.owner(), _owner, "Owner not set");
+
+        assertTrue(
+            _hcCapacitor.hasRole(_hcCapacitor.SOCKET_ROLE(), _socket),
+            "Socket role not set"
+        );
+
+        _assertPacketById(bytes32(0), 0);
+        _assertPacketToBeSealed(bytes32(0), 0);
+    }
+
+    function testSetSocket() external {
+        address newSocket = address(8);
+        vm.expectRevert(Ownable.OnlyOwner.selector);
+        _hcCapacitor.setSocket(newSocket);
+
+        hoax(_owner);
+        _hcCapacitor.setSocket(newSocket);
+        assertTrue(_hcCapacitor.hasRole(_hcCapacitor.SOCKET_ROLE(), newSocket));
+    }
+
+    function testAddMessage() external {
+        _addPackedMessage(_message_0);
+        _assertPacketById(_message_0, 0);
+        _assertPacketToBeSealed(_message_0, 0);
+    }
+
+    function testSealPacket() external {
+        vm.expectRevert(BaseCapacitor.NoPendingPacket.selector);
+        _sealPacket();
+
+        _addPackedMessage(_message_0);
+
+        _sealPacket();
+        _assertPacketById(_message_0, 0);
+        _assertPacketById(bytes32(0), 1);
+        _assertNextPacket(bytes32(0), 1);
+    }
+
+    function testAddWithoutSeal() external {
+        _addPackedMessage(_message_0);
+        _addPackedMessage(_message_1);
+    }
+
+    function testAddMessageMultiple() external {
+        _addPackedMessage(_message_0);
+        _addPackedMessage(_message_1);
+        _addPackedMessage(_message_2);
+
+        assertEq(_hcCapacitor.getLatestPacketCount(), 0);
+        (, uint256 packetToSeal) = _hcCapacitor.getNextPacketToBeSealed();
+        assertEq(packetToSeal, 0);
+
+        _sealPacket();
+        (, packetToSeal) = _hcCapacitor.getNextPacketToBeSealed();
+        assertEq(packetToSeal, 1);
+
+        bytes32 hashedRoot = keccak256(abi.encode(bytes32(0), _message_0));
+
+        hashedRoot = keccak256(abi.encode(hashedRoot, _message_1));
+
+        hashedRoot = keccak256(abi.encode(hashedRoot, _message_2));
+
+        bytes32 root = _hcCapacitor.getRootById(0);
+        assertEq(root, hashedRoot);
+
+        roots.push(_message_0);
+        roots.push(_message_1);
+        roots.push(_message_2);
+        bytes memory proof = abi.encode(roots);
+        assertTrue(
+            _hcDecapacitor.verifyMessageInclusion(hashedRoot, _message_0, proof)
+        );
+        assertTrue(
+            _hcDecapacitor.verifyMessageInclusion(hashedRoot, _message_1, proof)
+        );
+        assertTrue(
+            _hcDecapacitor.verifyMessageInclusion(hashedRoot, _message_2, proof)
+        );
+    }
+
+    function testAddMessageByRaju() external {
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                AccessControl.NoPermit.selector,
+                _hcCapacitor.SOCKET_ROLE()
+            )
+        );
+        hoax(_raju);
+        _hcCapacitor.addPackedMessage(_message_0);
+    }
+
+    function testSealPacketByRaju() external {
+        _addPackedMessage(_message_0);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                AccessControl.NoPermit.selector,
+                _hcCapacitor.SOCKET_ROLE()
+            )
+        );
+        hoax(_raju);
+        _hcCapacitor.sealPacket();
+    }
+
+    function _assertPacketToBeSealed(bytes32 root_, uint256 packetId_) private {
+        (bytes32 root, uint256 packetId) = _hcCapacitor
+            .getNextPacketToBeSealed();
+        assertEq(packetId, packetId_, "packetId Invalid");
+    }
+
+    function _assertNextPacket(bytes32 root_, uint256 packetId_) private {
+        uint256 nextPacketId = _hcCapacitor.getLatestPacketCount() + 1;
+        bytes32 root = _hcCapacitor.getRootById(nextPacketId);
+        assertEq(root, root_, "Root Invalid");
+        assertEq(nextPacketId, packetId_, "packetId Invalid");
+    }
+
+    function _assertPacketById(bytes32 root_, uint256 packetId_) private {
+        bytes32 root = _hcCapacitor.getRootById(packetId_);
+        bytes32 packedRoot = root_;
+        if (root != bytes32(0))
+            packedRoot = keccak256(abi.encode(bytes32(0), root_));
+        assertEq(root, packedRoot, "Root Invalid");
+    }
+
+    function _addPackedMessage(bytes32 packedMessage) private {
+        hoax(_socket);
+        _hcCapacitor.addPackedMessage(packedMessage);
+    }
+
+    function _sealPacket() private returns (bytes32 root, uint256 packetId) {
+        hoax(_socket);
+        (root, packetId) = _hcCapacitor.sealPacket();
+    }
+}
