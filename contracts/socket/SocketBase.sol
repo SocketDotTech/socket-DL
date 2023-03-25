57dc2c0c770f5a0ce45da76005b779327f28b40b
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-03-25 23:55:20 +0530

    feat: execute manager setter

--- contracts/socket/SocketBase.sol
+++ contracts/socket/SocketBase.sol
@@ -18,13 +18,13 @@ abstract contract SocketBase is SocketConfig, ReentrancyGuard {
     error InvalidAttester();
 
     event HasherSet(address hasher);
+    event ExecutionManagerSet(address executionManager);
 
     function setHasher(address hasher_) external onlyOwner {
         hasher__ = IHasher(hasher_);
         emit HasherSet(hasher_);
     }
 
-    // open issue #50
     /**
      * @notice updates transmitManager_
      * @param transmitManager_ address of Transmit Manager
@@ -32,5 +32,14 @@ abstract contract SocketBase is SocketConfig, ReentrancyGuard {
     function setTransmitManager(address transmitManager_) external onlyOwner {
         transmitManager__ = ITransmitManager(transmitManager_);
         emit TransmitManagerSet(transmitManager_);
+    }
+
+    /**
+     * @notice updates executionManager_
+     * @param executionManager_ address of Execution Manager
+     */
+    function setExecutionManager(address executionManager_) external onlyOwner {
+        executionManager__ = IExecutionManager(executionManager_);
+        emit ExecutionManagerSet(executionManager_);
     }
 }
