ef68f6629b12790b2b3ccaab26b688baa2b3b9c1
kanth <kanth.explorer@gmail.com>
2023-03-26 20:03:59 +0530

    feat: add SocketRole constant to AccessRoles

--- contracts/utils/AccessRoles.sol
+++ contracts/utils/AccessRoles.sol
@@ -9,3 +9,4 @@ bytes32 constant GOVERNANCE_ROLE = keccak256("GOVERNANCE_ROLE");
 bytes32 constant GAS_LIMIT_UPDATER_ROLE = keccak256("GAS_LIMIT_UPDATER_ROLE");
 bytes32 constant TRANSMITTER_ROLE = keccak256("TRANSMITTER_ROLE");
 bytes32 constant WATCHER_ROLE = keccak256("WATCHER_ROLE");
+bytes32 constant SOCKET_ROLE = keccak256("SOCKET_ROLE");
