7effc415a01ebd5f1b75bcf24ec57f4f901bc21d
akash <akash.minato@gmail.com>
2023-04-15 16:01:41 +0530

    added signer code

--- scripts/native-bridge-helpers/polygon/l2tol1Relay.ts
+++ scripts/native-bridge-helpers/polygon/l2tol1Relay.ts
@@ -6,18 +6,71 @@ use(Web3ClientPlugin);
 
 import { chainSlugs, getJsonRpcUrl } from "../../constants";
 import { deployedAddressPath, getInstance } from "../../deploy/utils";
+import { Provider, TransactionRequest, TransactionResponse } from "@ethersproject/abstract-provider";
+import {Signer} from "@ethersproject/abstract-signer";
+import { Bytes } from "@ethersproject/bytes";
+import { Deferrable } from "@ethersproject/properties";
+import { Transaction } from '@ethersproject/transactions';
+import axios from "axios";
+
+type ProviderWithWrapTransaction = Provider & { _wrapTransaction(tx: Transaction, hash?: string): TransactionResponse };
+
+const axiosPost = async (url:string, data:any, config:object = {}) => {
+	try {
+		let response = await axios.post(url, data, config);
+		return {success:true,  ...response.data};
+	} catch (error:any) {
+		console.log("status : ", error?.response?.status);
+		return {success:false, ...error?.response?.data} ;
+	}
+}
+
+export class SocketRelaySigner extends Signer {
+    constructor (readonly provider:Provider, readonly relayUrl:string, readonly sequential:boolean = false) {
+        super()
+    }
+
+    public async getAddress(): Promise<string> {
+        // some random address
+        return "0x5367Efc17020Aa1CF0943bA7eD17f1D3e4c7d7EE";
+    }
+    public async signMessage(message: string | Bytes): Promise<string> {
+        throw new Error(" signMessage not Implemented");
+    }
+    public connect(provider: Provider): Signer {
+        return new SocketRelaySigner(provider, this.relayUrl, this.sequential);
+    }
+    public async signTransaction(transaction: Deferrable<TransactionRequest>): Promise<string> {
+        throw new Error(" signTransaction not Implemented");
+    }
+    public async sendTransaction(transaction: Deferrable<TransactionRequest>): Promise<TransactionResponse> {
+        let payload = {
+            chainId:(await this.provider.getNetwork()).chainId,
+            sequential:this.sequential,
+            ...transaction
+        };
+        console.log("sendTransaction in signer: ", payload);
+        let result = await axiosPost(this.relayUrl, payload);
+        if (!result.success) throw result;
+        let tx = result?.data as Transaction;
+        return (this.provider as ProviderWithWrapTransaction)._wrapTransaction(tx);
+    }
+}
 
 // get providers for source and destination
-const privateKey = process.env.DEVNET_PRIVKEY;
-const sealTxHash = "";
+const privateKey = "0x8c5261668696ce22758910d05bab8f186d6eb247ceac2af2e82c7dc17669b036";
+// const privateKey = process.env.DEVNET_PRIVKEY;
+const sealTxHash = "0x8c5261668696ce22758910d05bab8f186d6eb247ceac2af2e82c7dc17669b036";
 
 const localChain = "polygon-mumbai";
 const remoteChain = "goerli";
 
-const l1Provider = new providers.JsonRpcProvider(getJsonRpcUrl(localChain));
-const l2Provider = new providers.JsonRpcProvider(getJsonRpcUrl(remoteChain));
-const l1Wallet = new Wallet(privateKey, l1Provider);
-const l2Wallet = new Wallet(privateKey, l2Provider);
+const l2Provider = new providers.JsonRpcProvider(getJsonRpcUrl(localChain));
+const l1Provider = new providers.JsonRpcProvider(getJsonRpcUrl(remoteChain));
+const l1Wallet = new Wallet(privateKey, l2Provider);
+const l2Wallet = new Wallet(privateKey, l1Provider);
+const l1Signer = new SocketRelaySigner(l1Provider, "https://google.com");
+const l2Signer = new SocketRelaySigner(l2Provider, "https://google.com");
 
 export const main = async () => {
   try {
@@ -32,31 +85,33 @@ export const main = async () => {
     ) {
       throw new Error("Deployed Addresses not found");
     }
-    const l2Config = addresses[chainSlugs[remoteChain]];
+    const l1Config = addresses[chainSlugs[remoteChain]];
+
+    const ABI = ["function receiveMessage(bytes memory receivePacketProof)"];
 
     // get socket contracts for both chains
     // counter l1, counter l2, seal, execute
     const l1Switchboard = (
       await getInstance(
         "PolygonL1Switchboard",
-        l2Config["integrations"][chainSlugs[remoteChain]]["switchboard"]
+        l1Config["integrations"][chainSlugs[localChain]]["switchboard"]
       )
-    ).connect(l2Wallet);
+    ).connect(l1Signer);
 
     const posClient = new POSClient();
     await posClient.init({
       network: "testnet",
       version: "mumbai",
       parent: {
-        provider: l2Wallet, //new HDWalletProvider(privateKey, parentRPC),
+        provider: l2Signer, //new HDWalletProvider(privateKey, parentRPC),
         defaultConfig: {
-          from: l2Wallet.address,
+          from: "0x5367Efc17020Aa1CF0943bA7eD17f1D3e4c7d7EE",
         },
       },
       child: {
-        provider: l1Wallet, //new HDWalletProvider(privateKey, childRPC),
+        provider: l1Signer, //new HDWalletProvider(privateKey, childRPC),
         defaultConfig: {
-          from: l1Wallet.address,
+          from: "0x5367Efc17020Aa1CF0943bA7eD17f1D3e4c7d7EE",
         },
       },
     });
@@ -74,6 +129,7 @@ export const main = async () => {
       false
     );
 
+    console.log("proof: ", proof);
     const tx = await l1Switchboard.receiveMessage(proof);
     await tx.wait();
   } catch (error) {
