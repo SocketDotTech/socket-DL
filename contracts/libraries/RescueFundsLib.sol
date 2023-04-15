48ca53943666127d5bd1a5d9d6eb84d03685ae86
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-04-15 14:39:54 +0530

    fix: imports

--- contracts/libraries/RescueFundsLib.sol
+++ contracts/libraries/RescueFundsLib.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.7;
 
-import "../libraries/SafeTransferLib.sol";
+import "./SafeTransferLib.sol";
 
 library RescueFundsLib {
     using SafeTransferLib for IERC20;
