96fd88f91c9bb53d5a323c64df6757805c3a61a8
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-05-01 05:08:13 +0530

    doc: interface and utils

--- contracts/libraries/FeesHelper.sol
+++ contracts/libraries/FeesHelper.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.7;
 
+/**
+ * @title FeesHelper
+ * @dev A library for managing fee collection and distribution.
+ */
 library FeesHelper {
     error TransferFailed();
     event FeesWithdrawn(address account, uint256 amount);
 
-    // TODO: to support fee distribution
     /**
-     * @notice transfers the fees collected to `account_`
-     * @param account_ address to transfer ETH
+     * @dev Transfers the fees collected to the specified address.
+     * @dev This contract will be further developed to support fee distribution to various participants of the system
+     * @notice The caller of this function must have the required funds.
+     * @param account_ The address to transfer ETH to.
      */
     function withdrawFees(address account_) internal {
         require(account_ != address(0));
