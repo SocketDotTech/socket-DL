75c469aefcb676ed6ada0e4e4217d30e5f4a8a2a
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-05-01 15:01:45 +0530

    docs: natspecs

--- contracts/utils/AccessControlExtended.sol
+++ contracts/utils/AccessControlExtended.sol
@@ -3,7 +3,19 @@ pragma solidity 0.8.7;
 
 import "./AccessControl.sol";
 
+/**
+ * @title AccessControlExtended
+ * @dev This contract extends the functionality of the AccessControl contract by adding
+ * the ability to grant and revoke roles based on a combination of role name and a chain slug.
+ * It also provides batch operations for granting and revoking roles.
+ */
 contract AccessControlExtended is AccessControl {
+    /**
+     * @dev Modifier that restricts access to addresses having roles with the given role name and chain slug.
+     * Throws an error if the caller does not have permit.
+     * @param roleName_ The name of the role.
+     * @param chainSlug_ The chain slug associated with the role.
+     */
     modifier onlyRoleWithChainSlug(
         string memory roleName_,
         uint256 chainSlug_
@@ -13,8 +25,17 @@ contract AccessControlExtended is AccessControl {
         _;
     }
 
+    /**
+     * @dev Constructor that sets the owner of the contract.
+     */
     constructor(address owner_) AccessControl(owner_) {}
 
+    /**
+     * @dev Grants a role to an address based on the role name and chain slug.
+     * @param roleName_ The name of the role.
+     * @param chainSlug_ The chain slug associated with the role.
+     * @param grantee_ The address to be granted the role.
+     */
     function grantRole(
         string memory roleName_,
         uint256 chainSlug_,
@@ -23,6 +44,11 @@ contract AccessControlExtended is AccessControl {
         _grantRole(roleName_, chainSlug_, grantee_);
     }
 
+    /**
+     * @dev Grants multiple roles to multiple addresses in batch.
+     * @param roleNames_ The names of the roles to grant.
+     * @param grantees_ The addresses to be granted the roles.
+     */
     function grantBatchRole(
         bytes32[] calldata roleNames_,
         address[] calldata grantees_
@@ -32,6 +58,11 @@ contract AccessControlExtended is AccessControl {
             _grantRole(roleNames_[index], grantees_[index]);
     }
 
+    /**
+     * @dev Revokes multiple roles from multiple addresses in batch.
+     * @param roleNames_ The names of the roles to revoke.
+     * @param grantees_ The addresses to be revoked the roles.
+     */
     function revokeBatchRole(
         bytes32[] calldata roleNames_,
         address[] calldata grantees_
@@ -49,6 +80,13 @@ contract AccessControlExtended is AccessControl {
         _grantRole(keccak256(abi.encode(roleName_, chainSlug_)), grantee_);
     }
 
+    /**
+     * @dev Checks if an address has a role based on the role name and chain slug.
+     * @param roleName_ The name of the role.
+     * @param chainSlug_ The chain slug associated with the role.
+     * @param address_ The address to check for the role.
+     * @return A boolean indicating whether the address has the specified role.
+     */
     function hasRole(
         string memory roleName_,
         uint256 chainSlug_,
@@ -65,6 +103,12 @@ contract AccessControlExtended is AccessControl {
         return _hasRole(keccak256(abi.encode(roleName_, chainSlug_)), address_);
     }
 
+    /**
+     * @dev Revokes roles from an address
+     * @param roleName_ The names of the roles to revoke.
+     * @param chainSlug_ The chain slug associated with the role.
+     * @param grantee_ The addresses to be revoked the roles.
+     */
     function revokeRole(
         string memory roleName_,
         uint256 chainSlug_,
