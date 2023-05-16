48f50c2d6310dcaf4edbad8937e8ad1f0fb5581a
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-05-17 01:42:05 +0530

    fix: bash script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 npx hardhat run scripts/deploy/index.ts && 
 npx ts-node scripts/deploy/checkRoles.ts && 
 npx hardhat run scripts/deploy/configure.ts && 
-npx hardhat run scripts/deploy/connect &&
+npx hardhat run scripts/deploy/connect.ts &&
 npx hardhat run scripts/deploy/verify.ts
