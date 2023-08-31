cd4442249a629d62a5a209f25676ed06ffbdd2ec
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-08-31 16:52:57 +0530

    fix: move config

--- scripts/socket-helpers/arb-estimate.ts
+++ scripts/socket-helpers/arb-estimate.ts
@@ -9,42 +9,8 @@ import {
 import { defaultAbiCoder } from "ethers/lib/utils";
 import { DeploymentMode, getAddresses } from "@socket.tech/dl-core";
 import PlugABI from "@socket.tech/dl-core/artifacts/abi/IPlug.json";
+import { addresses, dstChainSlug, srcChainSlug } from "./config";
 
-const addresses = {
-  "10": {
-    USDC: {
-      connectors: {
-        "2999": {
-          FAST: "0x1812ff6bd726934f18159164e2927B34949B16a8",
-        },
-      },
-    },
-  },
-  "2999": {
-    USDC: {
-      connectors: {
-        "10": {
-          FAST: "0x7b9ed5C43E87DAFB03211651d4FA41fEa1Eb9b3D",
-        },
-        "42161": {
-          FAST: "0x73019b64e31e699fFd27d54E91D686313C14191C",
-        },
-      },
-    },
-  },
-  "42161": {
-    USDC: {
-      connectors: {
-        "2999": {
-          FAST: "0x69Adf49285c25d9f840c577A0e3cb134caF944D3",
-        },
-      },
-    },
-  },
-};
-
-const srcChainSlug = 2999;
-const dstChainSlug = 42161;
 const dstChainRPC = process.env.ARBITRUM_RPC;
 
 export const getArbitrumGasEstimate = async (
