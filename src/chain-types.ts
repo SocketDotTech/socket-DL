70ddadccadb5a1ddd03682745e6894cd184bdf07
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-11-08 23:42:28 +0530

    2.4.11-test.1

--- src/chain-types.ts
+++ src/chain-types.ts
@@ -130,6 +130,31 @@ export const ChainSlugToKey = {
   [ChainSlug.CDK_TESTNET]: HardhatChainName.CDK_TESTNET,
 };
 
+export const ChainSlugToId = {
+  [ChainSlug.ARBITRUM]: ChainId.ARBITRUM,
+  [ChainSlug.ARBITRUM_GOERLI]: ChainId.ARBITRUM_GOERLI,
+  [ChainSlug.ARBITRUM_SEPOLIA]: ChainId.ARBITRUM_SEPOLIA,
+  [ChainSlug.OPTIMISM]: ChainId.OPTIMISM,
+  [ChainSlug.OPTIMISM_GOERLI]: ChainId.OPTIMISM_GOERLI,
+  [ChainSlug.OPTIMISM_SEPOLIA]: ChainId.OPTIMISM_SEPOLIA,
+  [ChainSlug.BSC]: ChainId.BSC,
+  [ChainSlug.BSC_TESTNET]: ChainId.BSC_TESTNET,
+  [ChainSlug.MAINNET]: ChainId.MAINNET,
+  [ChainSlug.GOERLI]: ChainId.GOERLI,
+  [ChainSlug.SEPOLIA]: ChainId.SEPOLIA,
+  [ChainSlug.POLYGON_MAINNET]: ChainId.POLYGON_MAINNET,
+  [ChainSlug.POLYGON_MUMBAI]: ChainId.POLYGON_MUMBAI,
+  [ChainSlug.AEVO_TESTNET]: ChainId.AEVO_TESTNET,
+  [ChainSlug.AEVO]: ChainId.AEVO,
+  [ChainSlug.HARDHAT]: ChainId.HARDHAT,
+  [ChainSlug.AVALANCHE]: ChainId.AVALANCHE,
+  [ChainSlug.LYRA_TESTNET]: ChainId.LYRA_TESTNET,
+  [ChainSlug.LYRA]: ChainId.LYRA,
+  [ChainSlug.XAI_TESTNET]: ChainId.XAI_TESTNET,
+  [ChainSlug.SX_NETWORK_TESTNET]: ChainId.SX_NETWORK_TESTNET,
+  [ChainSlug.CDK_TESTNET]: ChainId.CDK_TESTNET,
+};
+
 export const TestnetIds: ChainSlug[] = [
   ChainSlug.GOERLI,
   ChainSlug.SEPOLIA,
