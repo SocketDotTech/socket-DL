5cb9f61ac9928408f05e072c9f6ab8ddaebc1cf5
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2023-11-17 15:33:58 +0530

    feat: script to add new chain

---
+++ scripts/deploy/single-click-deploy/updateConfig.ts
@@ -0,0 +1,80 @@
+import path from "path";
+import fs from "fs";
+import { ChainId } from "../../../src";
+
+const enumFolderPath = path.join(__dirname, `/../../../src/enums/`);
+
+export const update = async (
+  chainName: string,
+  chainId: number,
+  isMainnet: boolean
+) => {
+  if (!fs.existsSync(enumFolderPath)) {
+    throw new Error(`Folder not found! ${enumFolderPath}`);
+  }
+
+  const filteredChain = Object.values(ChainId).filter((c) => c == chainId);
+  if (filteredChain.length > 0) {
+    console.log("Chain already added!");
+    return;
+  }
+
+  await updateFile(
+    "hardhatChainName.ts",
+    `,\n  ${chainName.toUpperCase()} = "${chainName.toLowerCase()}",\n}\n`,
+    ",\n}"
+  );
+  await updateFile(
+    "chainId.ts",
+    `,\n  ${chainName.toUpperCase()} = ${chainId},\n}\n`,
+    ",\n}"
+  );
+  await updateFile(
+    "chainSlug.ts",
+    `,\n  ${chainName.toUpperCase()} = ChainId.${chainName.toUpperCase()},\n}\n`,
+    ",\n}"
+  );
+  await updateFile(
+    "chainSlugToKey.ts",
+    `,\n  [ChainSlug.${chainName.toUpperCase()}]: HardhatChainName.${chainName.toUpperCase()},\n}\n`,
+    ",\n}"
+  );
+  await updateFile(
+    "chainSlugToId.ts",
+    `,\n  [ChainSlug.${chainName.toUpperCase()}]: ChainId.${chainName.toUpperCase()},\n}\n`,
+    ",\n}"
+  );
+  await updateFile(
+    "hardhatChainNameToSlug.ts",
+    `,\n  [HardhatChainName.${chainName.toUpperCase()}]: ChainSlug.${chainName.toUpperCase()},\n}\n`,
+    ",\n}"
+  );
+
+  if (isMainnet) {
+    await updateFile(
+      "mainnetIds.ts",
+      `,\n  ChainSlug.${chainName.toUpperCase()},\n];\n`,
+      ",\n];"
+    );
+  } else
+    await updateFile(
+      "testnetIds.ts",
+      `,\n  ChainSlug.${chainName.toUpperCase()},\n];\n`,
+      ",\n];"
+    );
+};
+
+const updateFile = async (fileName, newChainDetails, replaceWith) => {
+  const filePath = enumFolderPath + fileName;
+  const outputExists = fs.existsSync(filePath);
+  if (!outputExists) throw new Error(`${fileName} enum not found! ${filePath}`);
+
+  const verificationDetailsString = fs.readFileSync(filePath, "utf-8");
+
+  // replace last bracket with new line
+  const verificationDetails = verificationDetailsString
+    .trimEnd()
+    .replace(replaceWith, newChainDetails);
+
+  fs.writeFileSync(filePath, verificationDetails);
+};
