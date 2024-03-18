ab3d92a213a5f9f84771a6c7f34766f906f06082
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2024-03-18 13:14:24 +0530

    feat: simulator contracts
    these contracts are replacement of original core contracts which can be used for fees estimation on different chains

---
+++ contracts/mocks/fee-updater/SimulatorUtils.sol
@@ -0,0 +1,66 @@
+pragma solidity 0.8.19;
+
+import "../../interfaces/IDecapacitor.sol";
+import "../../capacitors/SingleCapacitor.sol";
+import "../../switchboard/default-switchboards/FastSwitchboard.sol";
+import "../../utils/AccessRoles.sol";
+
+contract SimulatorUtils is AccessControlExtended {
+    ISocket public socket__;
+    ISignatureVerifier public signatureVerifier__;
+
+    error InsufficientMsgValue();
+
+    constructor(
+        address socket_,
+        address signatureVerifier_,
+        address signer_,
+        uint32 siblingSlug_
+    ) AccessControlExtended(msg.sender) {
+        socket__ = ISocket(socket_);
+        signatureVerifier__ = ISignatureVerifier(signatureVerifier_);
+        _grantRoleWithSlug(TRANSMITTER_ROLE, siblingSlug_, signer_);
+        _grantRole(EXECUTOR_ROLE, signer_);
+    }
+
+    // TM
+    function checkTransmitter(
+        uint32 siblingSlug_,
+        bytes32 digest_,
+        bytes calldata signature_
+    ) external view returns (address, bool) {
+        address transmitter = signatureVerifier__.recoverSigner(
+            digest_,
+            signature_
+        );
+        _hasRoleWithSlug(TRANSMITTER_ROLE, siblingSlug_, transmitter);
+
+        return (transmitter, true);
+    }
+
+    // // EM
+    function updateExecutionFees(address, uint128, bytes32) external view {
+        if (msg.sender != address(socket__)) return;
+    }
+
+    function verifyParams(
+        bytes32 executionParams_,
+        uint256 msgValue_
+    ) external pure {
+        uint256 params = uint256(executionParams_);
+        uint8 paramType = uint8(params >> 248);
+
+        if (paramType == 0) return;
+        uint256 expectedMsgValue = uint256(uint248(params));
+        if (msgValue_ < expectedMsgValue) revert InsufficientMsgValue();
+    }
+
+    function isExecutor(
+        bytes32 packedMessage,
+        bytes memory sig
+    ) external view returns (address executor, bool isValidExecutor) {
+        executor = signatureVerifier__.recoverSigner(packedMessage, sig);
+        _hasRole(EXECUTOR_ROLE, executor);
+        isValidExecutor = true;
+    }
+}
