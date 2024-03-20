d40f5fe539853bbcca2b3afdc3bf7b8b7ae2d341
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2024-03-21 01:13:14 +0530

    feat: fees updater utils

---
+++ src/enums/chain-native-token.ts
@@ -0,0 +1,55 @@
+import { ChainSlug } from "./chainSlug";
+
+export enum NativeTokens {
+  "ethereum" = "ethereum",
+  "matic-network" = "matic-network",
+  "binancecoin" = "binancecoin",
+  "sx-network-2" = "sx-network-2",
+  "mantle" = "mantle",
+}
+
+export const getCurrency = (chainSlug: ChainSlug) => {
+  switch (chainSlug) {
+    case ChainSlug.AEVO:
+    case ChainSlug.AEVO_TESTNET:
+    case ChainSlug.ANCIENT8_TESTNET2:
+    case ChainSlug.ARBITRUM:
+    case ChainSlug.ARBITRUM_GOERLI:
+    case ChainSlug.ARBITRUM_SEPOLIA:
+    case ChainSlug.BASE:
+    case ChainSlug.HOOK:
+    case ChainSlug.HOOK_TESTNET:
+    case ChainSlug.LYRA:
+    case ChainSlug.LYRA_TESTNET:
+    case ChainSlug.MAINNET:
+    case ChainSlug.MODE:
+    case ChainSlug.MODE_TESTNET:
+    case ChainSlug.OPTIMISM:
+    case ChainSlug.OPTIMISM_GOERLI:
+    case ChainSlug.OPTIMISM_SEPOLIA:
+    case ChainSlug.PARALLEL:
+    case ChainSlug.SEPOLIA:
+    // no gas needed for txs
+    case ChainSlug.REYA_CRONOS:
+    case ChainSlug.REYA:
+      return NativeTokens.ethereum;
+
+    case ChainSlug.BSC:
+    case ChainSlug.BSC_TESTNET:
+      return NativeTokens.binancecoin;
+
+    case ChainSlug.POLYGON_MAINNET:
+    case ChainSlug.POLYGON_MUMBAI:
+      return NativeTokens["matic-network"];
+
+    case ChainSlug.SX_NETWORK_TESTNET:
+    case ChainSlug.SX_NETWORK:
+      return NativeTokens["sx-network-2"];
+
+    case ChainSlug.MANTLE:
+      return NativeTokens.mantle;
+
+    default:
+      throw new Error("Invalid chainSlug");
+  }
+};
