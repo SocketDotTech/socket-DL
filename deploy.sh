798be24770ec26232b9c6f9ac5e6efcb6be7fa45
arthcp <arth.c.patel@gmail.com>
2024-04-25 17:41:26 +0530

    feat: use filter siblings

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 npx hardhat run scripts/deploy/1-deploy.ts  && 
 npx ts-node scripts/deploy/2-check-roles.ts --no-compile && 
 npx hardhat run scripts/deploy/3-configure.ts --no-compile && 
-npx hardhat run scripts/deploy/4-connect.ts --no-compile &&
+# npx hardhat run scripts/deploy/4-connect.ts --no-compile &&
 npx hardhat run scripts/deploy/5-verify.ts --no-compile
