9a51578b7709e88939712bcaddec523a22728cc8
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2024-06-18 22:58:28 +0530

    feat: dev s3 config upload

--- scripts/rpcConfig/constants/version.ts
+++ scripts/rpcConfig/constants/version.ts
@@ -1,6 +1,6 @@
 import { DeploymentMode } from "../../../src";
 
 export const version = {
-  [DeploymentMode.DEV]: "1.0.2",
+  [DeploymentMode.DEV]: "1.0.3",
   [DeploymentMode.PROD]: "1.0.22",
 };
