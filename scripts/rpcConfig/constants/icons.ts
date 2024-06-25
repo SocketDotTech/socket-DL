eab78e49346c500e3d9dafd870293e7445402d98
arthcp <arth.c.patel@gmail.com>
2024-06-25 17:12:43 +0530

    chore: lint

--- scripts/rpcConfig/constants/icons.ts
+++ scripts/rpcConfig/constants/icons.ts
@@ -6,7 +6,8 @@ export const icons = {
   [ChainSlug.HOOK]: "https://media.socket.tech/hook.jpg",
   [ChainSlug.MANTLE]: "https://media.socket.tech/mantle.png",
   [ChainSlug.REYA]: "https://media.socket.tech/reya.png",
-  [ChainSlug.BSC_TESTNET]: "https://testnet.bscscan.com/assets/bsc/images/svg/logos/logo-light.svg?v=24.5.5.0",
+  [ChainSlug.BSC_TESTNET]:
+    "https://testnet.bscscan.com/assets/bsc/images/svg/logos/logo-light.svg?v=24.5.5.0",
   [ChainSlug.POLYNOMIAL]: "",
   [ChainSlug.SYNDR]: "",
 };
