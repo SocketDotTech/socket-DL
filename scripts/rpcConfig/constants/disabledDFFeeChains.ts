4a084ddeb00464a9baf6696a59b6d194d89924bd
arthcp <arth.c.patel@gmail.com>
2024-07-22 19:52:56 +0400

    feat: disabledDFFeeChains list

---
+++ scripts/rpcConfig/constants/disabledDFFeeChains.ts
@@ -0,0 +1,20 @@
+import { ChainSlug } from "../../../src";
+
+export const disabledDFFeeChains = [
+  ChainSlug.AEVO,
+  ChainSlug.ARBITRUM,
+  ChainSlug.OPTIMISM,
+  ChainSlug.BSC,
+  ChainSlug.POLYGON_MAINNET,
+  ChainSlug.LYRA,
+  ChainSlug.MAINNET,
+  ChainSlug.MANTLE,
+  ChainSlug.HOOK,
+  ChainSlug.REYA,
+  ChainSlug.BASE,
+  ChainSlug.KINTO,
+  ChainSlug.WINR,
+  ChainSlug.BLAST,
+  ChainSlug.POLYNOMIAL,
+  ChainSlug.SYNDR,
+];
